00bafd286c8be112d7102fb2eaee76e33290df8a
Kushagra614 <kushagravardhan619@gmail.com>
2026-03-17 23:36:42 +0530

    LC125

--- Scratch_C++_DSA/Blind75/Valid Palindrome/code.cpp
+++ Scratch_C++_DSA/Blind75/Valid Palindrome/code.cpp
@@ -1,53 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool isPalindrome(string s)
-{
-    int n = s.size();
-    string t = "";
-  
-
-    for (int k = 0; k < n; k++)
-    {
-        if (isalpha(s[k]) && s[k] != ' ')
-        {
-            t += tolower(s[k]);
+bool isPalindrome(string s) {
+        int n = s.size();
+        int i = 0;
+        int j = n - 1;
+        while (i < j) {
+            if (!isalnum(s[i]))
+            {
+                i++;
+            }
+            else if(!isalnum(s[j]))
+            {
+                j--;
+            }
+            else
+            {
+                if(tolower(s[i]) == tolower(s[j]))
+                {
+                    i++;
+                    j--;
+                }
+                else
+                {
+                    return false;
+                }
+                
+            }
+            
         }
+        return true;
     }
-
-      int i = 0;
-    int j = t.size() - 1;
-    while (i < j)
-    {
-        if (t[i] == t[j])
-        {
-            i++;
-            j--;
-        }
-        else{
-            return false;
-        }
-    }
-    return true;
-}
 
 int main()
 {
     string s = "A man, a plan, a canal: Panama";
     int n = s.size();
-    // string t = "";
-    // for (int k = 0; k < n; k++)
-    // {
-    //     if (isalpha(s[k]) && s[k] != ' ')
-    //     {
-    //         t += tolower(s[k]);
-    //     }
-    // }
-
-    // cout<<t;
-
-    bool ans = isPalindrome(s);
-    cout<<ans;
+    if(isPalindrome(s))
+    {
+        cout<<"True\n";
+    }
+    else{
+        cout<<"False\n";
+    }
 
 
     return 0;
